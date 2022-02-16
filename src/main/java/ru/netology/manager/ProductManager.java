eb0855030a161467f137443e172c17dc299fd971
Vadim Uzhakin <vadimuzhakin@gmail.com>
2022-02-16 23:28:49 +0700

    Initial commit

--- src/main/java/ru/netology/manager/ProductManager.java
+++ src/main/java/ru/netology/manager/ProductManager.java
@@ -18,7 +18,12 @@ public class ProductManager {
         Product[] result = new Product[0]; // тут будем хранить подошедшие запросу продукты
         for (Product product : repository.findAll()) {
             if (matches(product, text)) {
-                return new Product[]{product};// "добавляем в конец" массива result продукт product
+                Product[] tmp = new Product[result.length + 1];
+                for (int i = 0; i < result.length; i++) {
+                    tmp[i] = result[i];
+                }
+                tmp[tmp.length - 1] = product;
+                result = tmp;
             }
         }
         return result;
