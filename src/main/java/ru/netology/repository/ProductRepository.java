f55d0a51db68e4d555ee3777ca8f1187cb63324e
Vadim Uzhakin <vadimuzhakin@gmail.com>
2022-02-19 20:47:07 +0700

    Initial commit

--- src/main/java/ru/netology/repository/ProductRepository.java
+++ src/main/java/ru/netology/repository/ProductRepository.java
@@ -1,25 +1,50 @@
 package ru.netology.repository;
 
 import ru.netology.domain.Product;
+import ru.netology.exception.AlreadyExistsException;
+import ru.netology.exception.NotFoundException;
 
 public class ProductRepository {
     private Product[] items = new Product[0];
 
     public void save(Product item) {
-        int length = items.length + 1;
-        Product[] tmp = new Product[length];
-        System.arraycopy(items, 0, tmp, 0, items.length);
-        int lastIndex = tmp.length - 1;
-        tmp[lastIndex] = item;
-        items = tmp;
-
+        for (Product product : findAll()) {
+            if (product.getId() == item.getId()) {
+                throw new AlreadyExistsException("Element with id: " + id + " not found");
+            } else {
+                int length = items.length + 1;
+                Product[] tmp = new Product[length];
+                System.arraycopy(items, 0, tmp, 0, items.length);
+                int lastIndex = tmp.length - 1;
+                tmp[lastIndex] = item;
+                items = tmp;
+            }
+        }
     }
+//        int result = items.length + 1;
+//        Product[] tmp = new Product[result];
+//        for (int i = 0; i < items.length; i++) {
+//            tmp[i] = items[i];
+//        }
+//        tmp[tmp.length - 1] = item;
+//        items = tmp;
+
+//    }
+//        int length = items.length + 1;
+//        Product[] tmp = new Product[length];
+//        System.arraycopy(items, 0, tmp, 0, items.length);
+//                int lastIndex = tmp.length - 1;
+//                tmp[lastIndex] = item;
+//                items = tmp;
 
     public Product[] findAll() {
         return items;
     }
 
-    public Product[] removeById(int id) {
+    public Product[] removeById(int id) throws NotFoundException {
+        if (findById(id) == null) {
+            throw new NotFoundException("Element with id: " + id + " not found");
+        }
         int length = items.length - 1;
         Product[] tmp = new Product[length];
         int index = 0;
@@ -31,6 +56,15 @@ public class ProductRepository {
         }
         items = tmp;
         return tmp;
+    }
+
+    public Product findById(int id) {
+        for (Product item : items) {
+            if (item.getId() == id) {
+                return item;
+            }
+        }
+        return null;
     }
 
 }
---
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -0,0 +1,77 @@
+package ru.netology.repository;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Book;
+import ru.netology.domain.Product;
+import ru.netology.domain.Smartphone;
+import ru.netology.exception.NotFoundException;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class ProductRepositoryTest {
+    private ProductRepository repo = new ProductRepository();
+    private Book coreJava = new Book(1, "core", 200, "first");
+    private Book whiteFang = new Book(2, "fang", 300, "first");
+    private Book blackFang = new Book(3, "fang", 550, "three");
+    private Smartphone oneX = new Smartphone(4, "one", 5000, "phone1");
+
+    @Test
+    void removeByIdAll() throws NotFoundException {
+        repo.save(coreJava);
+        repo.save(whiteFang);
+        repo.save(blackFang);
+        repo.save(oneX);
+
+        repo.removeById(1);
+        repo.removeById(2);
+        repo.removeById(3);
+        repo.removeById(4);
+
+        Product[] actual = repo.findAll();
+        Product[] expected = {};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void removeByIdNotFound() throws NotFoundException {
+        repo.save(coreJava);
+        repo.save(whiteFang);
+        repo.save(blackFang);
+        repo.save(oneX);
+
+        Assertions.assertThrows(NotFoundException.class, () -> repo.removeById(111));
+
+        Product[] actual = repo.findAll();
+        Product[] expected = {coreJava, whiteFang, blackFang, oneX};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void removeByIdOneProd() {
+        repo.save(coreJava);
+        repo.save(whiteFang);
+        repo.save(blackFang);
+        repo.save(oneX);
+
+        repo.removeById(2);
+
+        Product[] actual = repo.findAll();
+        Product[] expected = {coreJava, blackFang, oneX};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void saveByIdOneProd() {
+        repo.save(whiteFang);
+
+        Product[] actual = repo.findAll();
+        Product[] expected = {whiteFang};
+
+        assertArrayEquals(expected, actual);
+    }
+
+}
